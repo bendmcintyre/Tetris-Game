32709e0022a2509ebf80a0b69725cb2e900d0126
Ben McIntyre <ben@iPad.localdomain>
2022-10-24 12:49:49 -0400

    Start game and end game functions created

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//Declaring necessary variable to make the game run
 const playerX = "x";
 const playerO = "o";
 const winningPatterns = [
@@ -10,10 +11,51 @@ const winningPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
-
-const dateCell = document.querySelectorAll("[data-cell]");
+const dataCell = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
 const winningMessage = document.getElementById("winning-message");
 const restartButton = document.getElementById("restart-button");
 const winningMessageText = document.getElementById("winning-message-text");
-let isPlayerOTurn = false;
+let playerOTurn = false;
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+//This function below starts a new game
+function startGame() {
+  playerOTurn = false;
+  dataCell.forEach((cell) => {
+    cell.classList.remove(playerX);
+    cell.classList.remove(playerO);
+    cell.removeEventListener("click", cellClick);
+    cell.addEventListener("click", cellClick, { once: true });
+  });
+  setBoard();
+  winningMessage.classList.remove("show");
+}
+
+//This function below allows users to click spots on the grid
+function cellClick(e) {
+  const cell = e.target;
+  const currentClass = playerOTurn ? playerO : playerX;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    changeTurns();
+    setBoard();
+  }
+}
+
+//This funciton ends the game and determines a winner
+function endGame(draw) {
+  if (draw) {
+    winningMessageText.innerText = "Draw!";
+  } else {
+    winningMessageText.innerText = `${playerOTurn ? "O's" : "X's"} wins!`;
+  }
+  winningMessageText.classList.add("show");
+}
